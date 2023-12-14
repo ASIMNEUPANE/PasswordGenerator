9e42513d04e6c2d326168b4fbb0edc04bf78877d
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-14 18:00:13 +0545

    Fix styling issue in App component and add deployment configuration

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
 
   return (
     <>
-      <div className="  w-full max-w-lg mx-auto shadow-md rounded-lg px-4 my-8 text-orange=500 bg-gray-700">
+      <div className=" w-full max-w-lg mx-auto shadow-md rounded-lg px-4 my-8 text-orange=500 bg-gray-700">
         <h1 className="text-white text-center p-4 ">Password Generator</h1>
         <div className='className="flex shadow rounded-lg overflow-hidden mb-4'>
           <input
